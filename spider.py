bf654d8ca31fe02a83dabd4fc7ccc428c0cee37d
lsyiverson <lsyiverson@gmail.com>
2017-01-19 22:44:34 +0800

    [lsyiverson] - write weather result into excel 2007 format

--- spider.py
+++ spider.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # encoding=utf-8
 import urllib2
 from lxml import etree
 from datetime import datetime, timedelta
 import re
 from utils.fileutils import mkdir, touch ,append, rmtree
+from openpyxl import Workbook
 
 rmtree('./result')
 pattern = re.compile('\s+')
@@ -14,6 +17,11 @@ homePage = urllib2.urlopen(site + '/lishi/').read().decode('gbk').encode('utf8')
 homePageHtml = etree.HTML(homePage, parser=etree.HTMLParser(encoding='utf8'))
 
 provinceElementXPath = homePageHtml.xpath('//div[@id="content"]/div[@class="citychk"]/dl')
+
+workbook = Workbook()
+cnWeatherWorksheet = workbook.active
+cnWeatherWorksheet.title = 'China Weather'
+cnWeatherWorksheet.append(['省份', '城市', '日期', '日间天气', '夜间天气', '最高温度', '最低温度', '日间风力风向', '夜间风力风向'])
 
 for provinceElmTree in provinceElementXPath:
     # provinceElmTree = provinceElementXPath[2]
@@ -43,16 +51,31 @@ for provinceElmTree in provinceElementXPath:
             monthlyData = ''
             for dailyWeatherElmTree in cityMonthWeatherContentXPath[1:]:
                 date = re.sub(pattern, '', dailyWeatherElmTree.findall('td')[0].find('a').text)
+
                 weather = re.sub(pattern, '', dailyWeatherElmTree.findall('td')[1].text)
+                weatherSet = weather.split('/')
+                dayWeather = weatherSet[0]
+                nightWeather = weatherSet[1]
+
                 temperature = re.sub(pattern, '', dailyWeatherElmTree.findall('td')[2].text)
+                tempSet = temperature.split('/')
+                highestTemp = tempSet[0]
+                lowestTemp = tempSet[1]
+
                 wind = re.sub(pattern, '', dailyWeatherElmTree.findall('td')[3].text)
+                windSet = wind.split('/')
+                dayWind = windSet[0]
+                nightWind = windSet[1]
+
+                cnWeatherWorksheet.append([provinceName, cityName, date, dayWeather, nightWeather, highestTemp, lowestTemp, dayWind, nightWind])
 
                 format = '%s %s %s %s'
                 values = (date, weather, temperature, wind)
                 dailyWeatherData = format % values
                 monthlyData += dailyWeatherData + '\n'
             append(filepath, monthlyData)
-            print provinceName + cityName + monthstr
+            print provinceName + cityName + monthstr + u' 下载成功'
+workbook.save('./result/weather.xlsx')
 
 
 
